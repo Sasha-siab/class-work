312f5059988f2ca4683d7a18c96741c44922f027
Aleksandra Siabriuk <alexsbrk91@gmail.com>
2018-03-10 16:31:58 -0500

    new

--- jukebox/script/script.js
+++ jukebox/script/script.js
@@ -28,7 +28,9 @@ var object2 = new MusicItem(audioFiles[2],tracks[2],2);
 var object3 = new MusicItem(audioFiles[3],tracks[3],3);
 
 var currentlyPlaying;
+var currentlyPlayingIndex;
 
+var songs = [object0, object1, object2, object3];
 
 
 // ________________________________________PLAY BUTTON
@@ -40,6 +42,7 @@ playButton.addEventListener("click", function() {
   }else{
     object0.music.play();
     currentlyPlaying = object0.music;
+    currentlyPlayingIndex = object0.index;
   }
 })
 
@@ -60,27 +63,84 @@ stopButton.addEventListener("click", function () {
 })
 
 //_________________________________________PREV BUTTON
+// get an index of a current playing songs
+// check if the index is less than 0, if it is  - play last song
+//  if it is not - decrease index by 1 and play it
 
 var prevButton =document.getElementById("prev");
 prevButton.addEventListener("click", function () {
-  currentlyPlaying.pause();
-  //make a loop that will take prev item and play it
+  if (!currentlyPlaying) {  // check if something is not  playing
+    object0.music.play(); // if nothing is playing  go to the first song
+    currentlyPlaying = object0.music;
+    currentlyPlayingIndex = object0.index;
+    return; // get out of the function
+  }
+  currentlyPlaying.pause(); // stops playing song
+  if (currentlyPlayingIndex <= 0) {  // checking current index
+    currentlyPlaying = window['object' + (songs.length-1)].music ; //  send us to the last song
+    currentlyPlayingIndex = songs.length - 1;  //  changes index  to the last song
+    currentlyPlaying.play();
+  }else{    // if the index more than 0
+    currentlyPlayingIndex--;  // decrease  index by 1
+    currentlyPlaying = window['object'+currentlyPlayingIndex].music; // store new song
+    currentlyPlaying.play();
+
+  }
+
 })
 
 
 // // ___________________________________________NEXT BUTTON
+// get an index of a current playing songs
+// check if the index is less than length , if it is play first song
+//  if it is not - increase index by 1 and play it
+
 var nextButton = document.getElementById("next");
 nextButton.addEventListener("click", function () {
-  if (currentlyPlaying){
-    currentlyPlaying.pause();
-    currentlyPlaying.currentTime = 0;
+  if (!currentlyPlaying) {  // check if something is not  playing
+    object0.music.play(); // if nothing is playing  go to the first song
+    currentlyPlaying = object0.music;
+    currentlyPlayingIndex = object0.index;
+    return; // get out of the function
   }
-  if (trackIndex === 0) {
-                trackIndex = tracks.length - 1;
-            } else {
-                trackIndex = trackIndex - 1;
-            }
+  currentlyPlaying.pause(); // stops playing song
+  if (currentlyPlayingIndex == (songs.length-1)){
+    object0.music.play(); // if nothing is playing  go to the first song
+    currentlyPlaying = object0.music;
+    currentlyPlayingIndex = object0.index;
+    return; // get out of the function
+  }else{
+    // currentlyPlaying.pause(); // stops playing song
+    currentlyPlayingIndex ++;
+    currentlyPlaying = window["object" + currentlyPlayingIndex].music;
+    currentlyPlaying.play();
+  }
 })
+
+// // ___________________________________________SHUFFLE  BUTTON
+// check currently playing song
+// if nothing - play random
+// if something - stop it and play random
+
+ var shuffleButton = document.getElementById("shuffle");
+ shuffleButton.addEventListener("click", function () {
+   var a = Math.floor(Math.random() * songs.length); //  random number limited by length of a arr
+   if (!currentlyPlaying) { // if nothing is playing
+     currentlyPlaying = window["object" + a].music; // getting a random song
+     currentlyPlaying.play();
+     return;
+   }
+   currentlyPlaying.pause();  // if something is playing
+   currentlyPlaying = window["object" + a].music;  // store new var
+   currentlyPlayingIndex = a; // grub index
+   currentlyPlaying.play();
+   return;
+ })
+
+
+
+
+
 
 // ___________________________________________CLICK SONGS ITEM
 // assign songs data to the box above the player
@@ -95,6 +155,6 @@ album1.innerHTML = tracks[i].dataset.album;
 time1.innerHTML = tracks[i].dataset.time;
 window['object'+i].music.play(); //plays
 currentlyPlaying = window["object"+i].music; // assigns for manipulating
-
+currentlyPlayingIndex = i;
 })
 }
